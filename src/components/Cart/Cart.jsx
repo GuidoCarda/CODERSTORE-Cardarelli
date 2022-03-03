456cc37f346176e2b04906f3886ef55babd65d58
Guido Cardarelli <guidocarda@hotmail.com>
2022-03-03 17:11:36 -0300

    Context bug fix 2nd try?

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,6 +1,5 @@
-import React from 'react'
+import React,{ useContext } from 'react'
 import { Link } from 'react-router-dom'
-import { useContext } from 'react/cjs/react.development'
 import { CartContext } from '../../context/CartContext'
 import { MdDeleteForever, MdOutlineClose, MdCancelPresentation } from 'react-icons/md'
 import './Cart.css'
